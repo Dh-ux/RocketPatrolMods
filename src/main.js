f2a16f2eb7bcf79eb1fb017209f15441d7392226
Lexin Xiong <lxiong19@ucsc.edu>
2021-06-25 11:23:00 -0700

    commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 let config = {
-	type: Phaser.AUTO,
+	type: Phaser.CANVAS,
 	width: 640,
 	height: 480,
 	scene: [Menu, Play]
 }
 let game = new Phaser.Game(config);
+
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
+
+let keyF, keyR, keyP, keyLEFT, keyRIGHT, keySPACE;
