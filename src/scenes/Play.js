24e4fc7a005cb64d530d7790e4b83a2793aa7551
Lexin Xiong <lxiong19@ucsc.edu>
2021-06-26 07:04:17 -0700

    commit

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -9,6 +9,8 @@ class Play extends Phaser.Scene {
 		this.load.image('starfield', './assets/starfield.png');
 	  }
 	create() {
+		// place tile sprite
+        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
 		this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
         // white borders
         this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
