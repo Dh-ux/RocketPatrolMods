301c45ec542d381e1cfd4c1807bb80e6253d4dce
Lexin Xiong <lxiong19@ucsc.edu>
2021-06-26 08:58:10 -0700

    commit

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -68,6 +68,9 @@ class Play extends Phaser.Scene {
 		if(this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)) {
             this.scene.restart();
         }
+		if(this.gameOver && Phaser.Input.Keyboard.JustDown(keyLEFT)) {
+            this.scene.start("menuScene");
+        }
 		this.starfield.tilePositionX -= 4;
 
 		if (!this.gameOver) {              
