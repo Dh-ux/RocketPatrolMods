8cd81dfa3baa87c7f690e66020958e25527408aa
Lexin Xiong <lxiong19@ucsc.edu>
2021-06-26 08:17:57 -0700

    commit

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -55,7 +55,7 @@ class Play extends Phaser.Scene {
 		this.scoreLeft = this.add.text(borderUISize + borderPadding, borderUISize + borderPadding*2, this.p1Score, scoreConfig);
 
 		scoreConfig.fixedWidth = 0;
-		this.clock = this.time.delayedCall(60000, () => {
+		this.clock = this.time.delayedCall(1000, () => {
 			this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
 			this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart or ← to Menu', scoreConfig).setOrigin(0.5);
 			this.gameOver = true;
