55fc602b2385fa4e807d2909269f8ac0bc58bc06
Lexin Xiong <lxiong19@ucsc.edu>
2021-06-26 08:38:30 -0700

    commit

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -4,7 +4,7 @@ class Menu extends Phaser.Scene {
 	}
 	preload() {
         // load audio
-        this.load.audio('sfx_select', './assets/assets_bilp_select12.wav');
+        this.load.audio('sfx_select', './assets/blip_select12.wav');
         this.load.audio('sfx_explosion', './assets/explosion38.wav');
         this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
     }
