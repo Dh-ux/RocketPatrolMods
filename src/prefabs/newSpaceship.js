3c8044cd6f4f7a3e9caf3998c8acab3ab783c09a
Lexin Xiong <lxiong19@ucsc.edu>
2021-06-29 14:54:55 -0700

    commit

--- src/prefabs/newSpaceship.js
+++ src/prefabs/newSpaceship.js
@@ -6,21 +6,35 @@ class Spaceship2 extends Phaser.GameObjects.Sprite {
         this.points = pointValue;   // store pointValue
 
     }
-
-    update() {
-
-        // move spaceship left
-        this.x -= 7;
-        // wraparound from left to right edge
-        if (this.x <= 0-this.width) {
-
-            this.reset();
+    update () {
+        // move spaceship 
+        if (this.mLeft) {
+            this.x -= 6;
+        } else {
+            this.x += 6
         }
+        // wrap around
+        if(this.mLeft && this.x <= 0 - this.width) {
+            this.x = game.config.width;
+        } else if (!this.mLeft && this.x > game.config.width) {
+            this.x = 0 - this.width;
+        }
+    }
+    reset () {
+        let random = Math.random()
+        this.mLeft = Math.floor(random * 2) == 0;
+        random = Math.abs(random - 0.5) * 2;
+        this.moveSpeed = Math.ceil(game.settings.spaceshipSpeed * random);
+        if (this.mLeft) {
+            this.flipX = false;
+            this.x = game.config.width + (borderUISize * random * 6);
+        } else {
+            this.flipX = true;
+            this.x = 0 - (borderUISize * random * 6);
+        }
+    }
 
-    }
-    reset(){
-        this.x = game.config.width;
-    }
+    
   
 
 }
