14281d50f471aafb94b281b0fdf4ac1fc4037040
Lexin Xiong <lxiong19@ucsc.edu>
2021-06-29 14:55:50 -0700

    commit

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@
 //Allow the player to control the Rocket after it's fired(5)
 //Add your own (copyright-free) background music to the Play scene(5)
 //Implement mouse control for player movement and mouse click to fire (20)
-//
+//Randomize each spaceship's movement direction at the start of each play (5)
 let config = {
 	type: Phaser.CANVAS,
 	width: 640,
