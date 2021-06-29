a0ce36c8127d6b237a6b1e51d64d3502b50b3afc
Lexin Xiong <lxiong19@ucsc.edu>
2021-06-29 09:13:50 -0700

    commit

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ let config = {
 }
 let game = new Phaser.Game(config);
 // set UI sizes
-let borderUISize = game.config.height / 20;
+let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
 
 // reserve keyboard variables
